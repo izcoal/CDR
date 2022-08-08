72f350df913bbd601d419eb06da0916cb5a68064
izcoal <izcoal8@gmail.com>
2022-08-08 18:16:57 +0200

    v 0.0.1.0
    Nueva version

--- R/tempmin_data.R
+++ R/tempmin_data.R
@@ -1,2 +1,12 @@
-# Aqui se describe el dataset
+#' Datos de temperaturas
+#'
+#' @format A data.table with 1066 rows and 5 variables:
+#' \describe{
+#'   \item{fecha}{chr Fecha}
+#'   \item{indicativo}{chr }
+#'   \item{tmin}{num Temperatura mínima}
+#'   \item{longitud}{num longitud}
+#'   \item{latitud}{num latitud}
+#' }
+#' @source ?????
 "tempmin_data"
---
+++ R/toledo.R
@@ -0,0 +1,15 @@
+#' Datos la provincia de Toledo
+#'
+#' @format A sf with 1 row and 8 variables:
+#' \describe{
+#'   \item{codauto}{factor Código de la CCAA}
+#'   \item{ine.ccaa.name}{factor Nombre de la CCAA}
+#'   \item{cpro}{factor Código de la provincia}
+#'   \item{ine.prov.name}{factor Nombre de la provincia}
+#'   \item{cmun}{factor Código del municipio}
+#'   \item{name}{factor Nombre del municipio}
+#'   \item{LAU_CODE}{factor}
+#'   \item{geom}{sfc_POLYGON}
+#' }
+#' @source ?????
+"toledo"
