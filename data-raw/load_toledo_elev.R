41d7680470cfb9135888f90a1a6d11260019a3ad
izcoal <izcoal8@gmail.com>
2022-08-16 17:15:27 +0200

    Update load_toledo_elev.R

--- data-raw/load_toledo_elev.R
+++ data-raw/load_toledo_elev.R
@@ -1,9 +1,8 @@
 # Load Toledo elev
 
 library(raster)
-filename <- system.file("external/Toledo_DEM.asc",package="CDR")
-elev <- raster(filename)
-#elev <- raster("data-raw/Toledo_DEM.asc") #raster(here::here("data-raw/Toledo_DEM.asc"))
+elev <- raster(here::here("data-raw/Toledo_DEM.asc"))
+elev@file@name <- system.file("external/Toledo_DEM.asc",package="CDR")
 usethis::use_data(elev, overwrite = TRUE)
 
 
