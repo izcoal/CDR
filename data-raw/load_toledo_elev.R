953272c5cad5a1c04bd4e0f0d3fca6867d5d18eb
izcoal <izcoal8@gmail.com>
2022-08-16 16:58:45 +0200

    Update load_toledo_elev.R

--- data-raw/load_toledo_elev.R
+++ data-raw/load_toledo_elev.R
@@ -1,8 +1,7 @@
 # Load Toledo elev
 
 library(raster)
-elev <- raster("https://github.com/izcoal/CDR/blob/main/data-raw/Toledo_DEM.asc")
-#elev <- raster(here::here("data-raw/Toledo_DEM.asc"))
+elev <- raster("data-raw/Toledo_DEM.asc") #raster(here::here("data-raw/Toledo_DEM.asc"))
 usethis::use_data(elev, overwrite = TRUE)
 
 
