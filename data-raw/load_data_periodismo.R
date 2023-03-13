d165e09525cd0c2c2d06a368aa7c4126ca76a8da
izcoal <izcoal8@gmail.com>
2023-03-13 17:55:42 +0100

    corregir data_periodismo

--- data-raw/load_data_periodismo.R
+++ data-raw/load_data_periodismo.R
@@ -1,6 +1,8 @@
 
 load(here::here("data-raw/data_periodismo.RData"))
 
+map_munis$geometry <- apply(map_munis, 1, function(x) unlist(unlist(x$geometry)))
+
 data_periodismo <- list(colors_bloques=colors_bloques,
                         datos_elecciones=datos_elecciones,
                         map_munis=map_munis)
