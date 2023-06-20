5110361f906a8699efbca2f783975ed9c3ff169e
izcoal <izcoal8@gmail.com>
2023-06-20 12:17:01 +0200

    Corrección retail

--- data-raw/load_datos_retail.R
+++ data-raw/load_datos_retail.R
@@ -1,4 +1,7 @@
 
 datos_retail <- read.csv2(here::here("data-raw/datos_retail.csv"),sep=",")
 
+datos_retail$fecha <- as.Date(datos_retail$fecha)
+datos_retail$importe_venta <- as.double(datos_retail$importe_venta)
+
 usethis::use_data(datos_retail, overwrite = TRUE)
